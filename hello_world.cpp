295f2feb6c6191c8fe7ac950d679d1830fb2a19d
Vishnyakov06 <147653545+Vishnyakov06@users.noreply.github.com>
2025-11-24 19:30:24 +0300

    Update hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -4,6 +4,6 @@ int main() {
 	string name;
 	std::cin >> name; // Read input from standard input
 	std::cout << "Hello, World from @" << name;// Output to standard output
-	std::cout << std::endl;// New line
+	std::cout << std::endl;// New dcdcdcdline
 	return 0;
 }
