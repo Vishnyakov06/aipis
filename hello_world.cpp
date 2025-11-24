a358d7e2a06ea1066dc325be7472cb93b8736fba
Your Name <147653545+Vishnyakov06@users.noreply.github.com>
2025-11-24 19:01:47 +0300

    docs: add comments

--- hello_world.cpp
+++ hello_world.cpp
@@ -2,8 +2,8 @@
 #include <string>
 int main() {
 	string name;
-	std::cin >> name;
-	std::cout << "Hello, World from @" << name;
-	std::cout << std::endl;
+	std::cin >> name; // Read input from standard input
+	std::cout << "Hello, World from @" << name;// Output to standard output
+	std::cout << std::endl;// New line
 	return 0;
 }
